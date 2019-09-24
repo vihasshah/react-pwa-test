43a539e4f0fc93514b974c7044f0358c400c7bb3
vihasshah <vihasshah@gmail.com>
2019-09-24 16:44:49 +0530

    Updates

---
+++ precache-manifest.58e37fdbf094f8e0b4d102b93cc87409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1207ddc1f62ee04ba811f9aa9903a3a",
+    "url": "/react-pwa-test/index.html"
+  },
+  {
+    "revision": "e4fafdd2cabfd64eff82",
+    "url": "/react-pwa-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7c16b25566ab6de8fc39",
+    "url": "/react-pwa-test/static/js/2.83b3b4ca.chunk.js"
+  },
+  {
+    "revision": "e4fafdd2cabfd64eff82",
+    "url": "/react-pwa-test/static/js/main.301a1618.chunk.js"
+  },
+  {
+    "revision": "baa4fd6784fb577055f4",
+    "url": "/react-pwa-test/static/js/runtime-main.fe799764.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-pwa-test/static/media/logo.25bf045c.svg"
+  }
+]);
